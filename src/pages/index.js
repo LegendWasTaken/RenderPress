248181295d437317549e2468bd4442b68eb5c63a
LegendWasTaken <caiodcw@gmail.com>
2023-05-28 23:11:34 +0800

    markdown rendering + post page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import About from "@/components/about";
 import BlogList from "@/components/bloglist";
+import {AiOutlineHome} from "react-icons/ai";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,8 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-          <div className={"flex gap-x-24 px-24 pt-24 h-screen"}>
-              <About/>
+          <div className={"flex gap-x-24 px-24 pt-12 h-screen"}>
+              <div className={"flex gap-x-2"}>
+                  <About/>
+                  <Link href={'/'}>
+                      <AiOutlineHome size={30}/>
+                  </Link>
+              </div>
               <div className={"flex-grow"}/>
               <div className={"overflow-y-scroll pb-4"}>
                   <BlogList/>
